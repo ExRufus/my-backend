b5d2e7696f1205fe6ea8cf4a962603aa7f7feb4e
ExRufus <akmaldyn02@gmail.com>
2023-09-07 20:08:20 +0700

    ini sudah selesai

--- config/config.js
+++ config/config.js
@@ -1,15 +1,18 @@
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   },
   test: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres'
   },
   production: {
     username: 'postgres', 
     password: 'AnakCompsie',
-    host: 'db.ffcjctqhmuwkqzylelku.supabase.co',
+    host: 'db.naixjrzxdtwmkmybldts.supabase.co',
     port: 6543,
     database: 'postgres',
+    dialect: 'postgres'
   }
 };
